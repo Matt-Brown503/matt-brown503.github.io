76ea3f92bece09c983c455788b20e4eb682486ec
Matt Brown <matt388@gmail.com>
2017-04-03 21:00:43 -0700

    refactored content fadein

--- js/main.js
+++ js/main.js
@@ -3,54 +3,50 @@ $(document).ready(function(){
 	$('header').addClass(' load');
 	$('.scroll').addClass(' load');
 
+	
+	
+	var myFunction = function(check) {
 		$(window).scroll(function() {
-	        if ($(window).scrollTop() + $(window).height() >= $('.aboutcheck').offset().top-100) {
-	        	$('.aboutcheck').parent().addClass(' slide');
-	        }
-    	});
-    	$(window).scroll(function() {
-	        if ($(window).scrollTop() + $(window).height() >= $('.workcheck').offset().top-100) {
-	        	$('.workcheck').parent().addClass(' slide');
-	        }
-    	});
-    	$(window).scroll(function() {
-	        if ($(window).scrollTop() + $(window).height() >= $('.studycheck').offset().top-100) {
-	        	$('.studycheck').parent().addClass(' slide');
-	        }
-    	});
-    	$(window).scroll(function() {
-	        if ($(window).scrollTop() + $(window).height() >= $('.contactcheck').offset().top-100) {
-	        	$('.contactcheck').parent().addClass(' slide');
-	        }
-    	});
-
-    	$(window).scroll(function() {
-	        if ($(window).scrollTop() + $(window).height() >= $('.badge-info').offset().top-300) {
-	        	$('.th-badge').addClass(' badge-animate');
-	        }
-    	});
-
-
-
-    	$('.desktop-button').click(function(){
-    		$('.a-group').toggleClass(' a-off');
-    		$('.b-group').toggleClass(' b-off');
-    	});
-    	
-    	$('.mobile-button').click(function(){
-    		$('.b-group').css('display', 'block');
-    		$(this).remove();
-    	});
-
-    	$('a[href^="#"').on('click', function(event) {
-			var target = $(this.getAttribute('href'));
-			if(target.length) {
-				event.preventDefault();
-				$('html, body').stop().animate({
-				scrollTop: target.offset().top-100
-				}, 550);
+			if ($(window).scrollTop() + $(window).height() >= $(check).offset().top-100) {
+				$(check).parent().addClass(' slide');
 			}
 		});
+	};
+
+	myFunction('.aboutcheck');
+
+	myFunction('.workcheck');
+
+	myFunction('.studycheck');
+	
+	myFunction('.contactcheck');
+
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() + $(window).height() >= $('.badge-info').offset().top-300) {
+			$('.th-badge').addClass(' badge-animate');
+		}
+	});
+
+	$('.desktop-button').click(function(){
+		$('.a-group').toggleClass(' a-off');
+		$('.b-group').toggleClass(' b-off');
+	});
+
+	$('.mobile-button').click(function(){
+		$('.b-group').css('display', 'block');
+		$(this).remove();
+	});
+
+	$('a[href^="#"').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if(target.length) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top-100
+			}, 550);
+		}
+	});
 	
 
 
@@ -62,134 +58,127 @@ $(document).ready(function(){
 	var date = [];
 	var mobileInfo = [];
 	$.getJSON(myUrl)
-	.done(function(data){
-		badgeList = data.badges;
-		var points = data.points.total;
-		var myBadges = badgeList.length-1;
-		badgeList =  badgeList.reverse();
-		rawDate = badgeList[0].earned_date;
-		for (var i = 0; i < 5; i++) {
-			var badgeName = badgeList[i].name;
-			var badgeIcon = badgeList[i].icon_url;
-			var badgeLink = badgeList[i].url;
+.done(function(data){
+	var badgeList = data.badges;
+	var points = data.points.total;
+	var myBadges = badgeList.length-1;
+	badgeList =  badgeList.reverse();
+	rawDate = badgeList[0].earned_date;
+	for (var i = 0; i < 5; i++) {
+		var badgeName = badgeList[i].name;
+		var badgeIcon = badgeList[i].icon_url;
+		var badgeLink = badgeList[i].url;
 
-			badgeHTML += "<a href="+badgeLink+" target='_blank'><img src="+badgeIcon+" class='th-badge badge-"+[i]+"'></a>";
+		badgeHTML += '<a href='+badgeLink+' target=\'_blank\'><img src='+badgeIcon+' class=\'th-badge badge-'+[i]+'\'></a>';
 
-			 badgeInfo.push(badgeName);
+		badgeInfo.push(badgeName);
 
-		
-		}
-		for (var i = 0; i < 10; i++) {
-			date.push(rawDate[i]);
-		}
-		date = date.join('');
 
-		for (var i = 0; i < badgeInfo.length; i++) {
-			mobileInfo += '<p>'+badgeInfo[i]+'</p>';
-		}
+	}
+	for (var i = 0; i < 10; i++) {
+		date.push(rawDate[i]);
+	}
+	date = date.join('');
 
-		$('#date').html(date);
-		$('#badges').html(badgeHTML);
-		$('#lessons').html(myBadges);
-		$('#points').html(points);
-		$('.badge-info-mobile').html(mobileInfo);
-		
-		
+	for (var i = 0; i < badgeInfo.length; i++) {
+		mobileInfo += '<p>'+badgeInfo[i]+'</p>';
+	}
+
+	$('#date').html(date);
+	$('#badges').html(badgeHTML);
+	$('#lessons').html(myBadges);
+	$('#points').html(points);
+	$('.badge-info-mobile').html(mobileInfo);
 		
 		
 		
+		
+		
 
-		var defaultText = '<p>Hover badge for description</p>';
-		$('.badge-0').mouseenter(function(){
+	var defaultText = '<p>Hover badge for description</p>';
+	$('.badge-0').mouseenter(function(){
 		$('.badge-info').animate({
 			width: 400,
 		},0, function(){
 			$('.badge-info').html('<p>'+badgeInfo[0]+'</p>');
 		
-			});
 		});
-		$('.badge-0').mouseleave(function(){
+	});
+	$('.badge-0').mouseleave(function(){
 		$('.badge-info').animate({
 			width: 300
 		},0, function(){
 			$('.badge-info').html(defaultText);
-			
-			});
-		});
 
-		$('.badge-1').mouseenter(function(){
+		});
+	});
+
+	$('.badge-1').mouseenter(function(){
 		$('.badge-info').animate({
 			width: 400
 		},0, function(){
 			$('.badge-info').html('<p>'+badgeInfo[1]+'</p>');
-		
-			});
+
 		});
-		$('.badge-1').mouseleave(function(){
+	});
+	$('.badge-1').mouseleave(function(){
 		$('.badge-info').animate({
 			width: 300
 		},0, function(){
 			$('.badge-info').html(defaultText);
-			
-			});
-		});
 
-		$('.badge-2').mouseenter(function(){
+		});
+	});
+
+	$('.badge-2').mouseenter(function(){
 		$('.badge-info').animate({
 			width: 400
 		},0, function(){
 			$('.badge-info').html('<p>'+badgeInfo[2]+'</p>');
 		
-			});
 		});
-		$('.badge-2').mouseleave(function(){
+	});
+	$('.badge-2').mouseleave(function(){
 		$('.badge-info').animate({
 			width: 300
 		},0, function(){
 			$('.badge-info').html(defaultText);
 		
-			});
 		});
+	});
 
-		$('.badge-3').mouseenter(function(){
+	$('.badge-3').mouseenter(function(){
 		$('.badge-info').animate({
 			width: 400
 		},0, function(){
 			$('.badge-info').html('<p>'+badgeInfo[3]+'</p>');
 			
-			});
 		});
-		$('.badge-3').mouseleave(function(){
+	});
+	$('.badge-3').mouseleave(function(){
 		$('.badge-info').animate({
 			width: 300
 		},0, function(){
 			$('.badge-info').html(defaultText);
 		
-			});
 		});
+	});
 
-		$('.badge-4').mouseenter(function(){
+	$('.badge-4').mouseenter(function(){
 		$('.badge-info').animate({
 			width: 400
 		},0, function(){
 			$('.badge-info').html('<p>'+badgeInfo[4]+'</p>');
 			
-			});
 		});
-		$('.badge-4').mouseleave(function(){
+	});
+	$('.badge-4').mouseleave(function(){
 		$('.badge-info').animate({
 			width: 300
 		},0, function(){
 			$('.badge-info').html(defaultText);
 		
-			});
 		});
-
-
 	});
-
-	
-
-
 });
-
+});
